25923f21944ac0f04b44fbf27a51500995d38c49
Scott Sheffield <geekwise.scott.sheffield@gmail.com>
2016-02-01 23:13:59 +0000

    configured app.js

--- static/app.js
+++ static/app.js
@@ -7,4 +7,28 @@ angular
               url: "/feed",
               templateUrl: "feed.html"
             })
+            .state('eDebate', {
+                url:"/eDebate",
+                templateUrl:"eDebate.html"
+            })
+            .state('login', {
+                url:"/login",
+                templateUrl:"login.html"
+            })
+            .state('news', {
+                url:"/news",
+                templateUrl:"news.html"
+            })
+            .state('PostView', {
+                url:"/postview",
+                templateUrl:"postview.html"
+            })
+            .state('signUp', {
+                url:"/signup",
+                templateUrl:"signup.html"
+            })
+            .state('user', {
+                url:"/user",
+                templateUrl:"user.html"
+            })
     });
