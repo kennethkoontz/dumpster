b0ca25bd994beff06b63789ddf6da3fce7d12fb5
Scott Sheffield <geekwise.scott.sheffield@gmail.com>
2016-02-05 22:53:19 +0000

    added feed and responsive angular content

--- static/edebate.controller.js
+++ static/edebate.controller.js
@@ -3,5 +3,8 @@ var app = angular.module('fed', []);
 angular
     .module('app')
     .controller('eDebateController', function($scope){
-        $scope.navigation = [{topic:'something something darkside'}]
-    })
+        $scope.navigation = [{topic:'angular.js worked finally'}]
+        
+        $scope.feed = [{text: 'howdy'}, {text: 'second post'}, {text: 'another one'}, {text: 'i appreciate that'},{text:'you is smawt'}]
+        
+    });
