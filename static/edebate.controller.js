b9f3833ecefa0c88c3648ca7f48faaa545da94e2
Scott Sheffield <geekwise.scott.sheffield@gmail.com>
2016-02-07 04:32:42 +0000

    minor cosmetic changes through out the app

--- static/edebate.controller.js
+++ static/edebate.controller.js
@@ -3,8 +3,11 @@ var app = angular.module('fed', []);
 angular
     .module('app')
     .controller('eDebateController', function($scope){
-        $scope.navigation = [{topic:'angular.js worked finally'}]
+        $scope.navigation = [{current:'angular.js worked finally'}]
+        $scope.pickTwo = [{optnOne: 'lemmings'}, {optnTwo: '2spooky4me'}]
         
-        $scope.feed = [{text: 'howdy'}, {text: 'second post'}, {text: 'another one'}, {text: 'i appreciate that'},{text:'you is smawt'}]
+        // series of posts / entries by users
+        $scope.feed = [{text: 'howdy'}, {text: 'second post'}, {text: 'another one'}, {text: 'i appreciate that'}, {text:'you is smawt'}]
+        
         
     });
