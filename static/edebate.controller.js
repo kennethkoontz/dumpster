7118a32abd73d173fc1643340a69f3e616945bfe
Scott Sheffield <geekwise.scott.sheffield@gmail.com>
2016-02-09 21:26:31 +0000

    eDebate controller adjustments

--- static/edebate.controller.js
+++ static/edebate.controller.js
@@ -9,5 +9,8 @@ angular
         // series of posts / entries by users
         $scope.feed = [{text: 'howdy'}, {text: 'second post'}, {text: 'another one'}, {text: 'i appreciate that'}, {text:'you is smawt'}]
         
+        $scope.response = [{like: '0', acknowledge: '0', dislike: '0', comment: '0'}]
+        // $scope.response = [{like: '2', acknowledge: '1', dislike: '0', comment: '8'}]
         
     });
+    
