fa3ef481e49ea4f1cdcb5e7d424a724bb2263ae6
geekwisecoltonbrockett <geekwise.colton.brockett@gmail.com>
2016-02-10 22:03:49 +0000

    fix feed error

--- static/feed.controller.js
+++ static/feed.controller.js
@@ -1,10 +1,3 @@
-var app = angular.module('fed', []);
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 5b9f0e6c02cb49d8c0636c71a3fb6f6899191dc2
 angular
     .module('app')
     .controller('FeedController', function($scope){
