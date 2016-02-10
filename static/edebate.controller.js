825079c225773cb317bec16c987c2ed3ed6109d7
Scott Sheffield <geekwise.scott.sheffield@gmail.com>
2016-02-10 23:50:39 +0000

    added profile image templating

--- static/edebate.controller.js
+++ static/edebate.controller.js
@@ -1,11 +1,17 @@
 angular
     .module('app')
     .controller('eDebateController', function($scope){
-        $scope.navigation = [{current:'angular.js worked finally'}]
+        $scope.navigation = [{current:'un-plebing code'}]
         $scope.pickTwo = [{optnOne: 'lemmings'}, {optnTwo: '2spooky4me'}]
         
         // series of posts / entries by users
-        $scope.feed = [{text: 'howdy'}, {text: 'second post'}, {text: 'another one'}, {text: 'i appreciate that'}, {text:'you is smawt'}]
+        $scope.feed = [
+            {text: 'howdy', profilePic: 'images/userProfileExample.png'},
+            {text: 'second post', profilePic: 'images/userProfileExample.png'},
+            {text: 'another one', profilePic: 'images/userProfileExample.png'},
+            {text: 'i appreciate that', profilePic: 'images/userProfileExample.png'},
+            {text:'you is smawt', profilePic: 'images/userProfileExample.png'}
+        ]
         
         $scope.response = [{like: '0', acknowledge: '0', dislike: '0', comment: '0'}]
         
