55bda9e0ae464419d0a77033f29a22fc7919df15
Scott Sheffield <geekwise.scott.sheffield@gmail.com>
2016-02-24 21:33:52 +0000

    added some minor functionality to a dropdown menu in signup.html

--- static/edebate.controller.js
+++ static/edebate.controller.js
@@ -19,7 +19,7 @@ angular
         };
         
         vm.addPost = function(){
-            // if(!vm.title || vm.title ==="") {return;};
+            if(!vm.title || vm.title === "") {return;};
             vm.feed.push({
                 text: vm.text ,
                 profilePic:'images/100x100profileImage.gif',
